0598a704701b754d9966c8be5ef7a73435123d2f
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-15 13:21:52 +0700

    Add debug mode

--- DetectorModels/src/data/data_loading.py
+++ DetectorModels/src/data/data_loading.py
@@ -6,6 +6,9 @@ from data.data_processing import *
 def load_data(csv_path, CONFIG):
     df = pd.read_csv(csv_path)
     
+    if CONFIG["debug"]:
+        df = df[:5000]
+    
     df = process_data(df)
     
     return df
--- DetectorModels/src/utils/utils.py
+++ DetectorModels/src/utils/utils.py
@@ -42,6 +42,7 @@ def parse_args():
     parser.add_argument("--num_classes", type=int, default=10, help="Number of classes")
     parser.add_argument("--n_fold", type=int, default=5, help="Number of folds")
     parser.add_argument("--fold", type=int, default=0, help="Fold")
+    parser.add_argument("--debug", action="store_true", help="Debug mode")
     
     args = parser.parse_args()
     CONFIG = vars(args)
