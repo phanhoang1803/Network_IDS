4f51430eaa7ddc003483fd69c1edd010dc5d8df1
Phan Van Hoang <phanhoang1803@gmail.com>
2024-07-30 19:36:47 +0700

    Update ckpts

--- DetectorModels/src/train_svm.py
+++ DetectorModels/src/train_svm.py
@@ -82,6 +82,7 @@ def main():
     df = load_data(train_csv, CONFIG)
     df, encoder, scaler = process_data(df)
 
+    make_dir(CONFIG["save_dir"])
     joblib.dump(encoder, os.path.join(CONFIG["save_dir"], "encoder.pkl"))
     joblib.dump(scaler, os.path.join(CONFIG["save_dir"], "scaler.pkl"))
 
